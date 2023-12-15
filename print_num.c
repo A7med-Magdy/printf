a82128e6fe808e160d7120b7f8d573304830fb74
A7med-Magdy <ahmed.magdy12mohammed@gmail.com>
2023-12-15 11:17:15 -0800

    eidt_2

--- print_num.c
+++ print_num.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(int c)
+int is_digit(char c)
 {
-	return (c >= '0' && c <= '9');
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
 }
 
 /**
